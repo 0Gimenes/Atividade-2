3d65d08f7021118dd1687a0b5d3845a7c4d6f202
0Gimenes <gabrielgimenes9353@gmail.com>
2023-10-19 14:16:38 -0400

    Criei a pagina do produto especifico,  e a const de produtos

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const produtos = [
+{id:1, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:2, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:3, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:4, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:5, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:6, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:7, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:8, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:9, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+{id:10, nome: "nome", preco:"400", descricao: "descricao", imagem:""},
+]
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -15,4 +28,8 @@ app.get('/tiagas', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+app.get('/albumb.ejs', (req, res) => {
+  res.render("albumb");
 });
